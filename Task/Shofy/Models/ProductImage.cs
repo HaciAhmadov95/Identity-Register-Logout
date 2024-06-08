775b2af6c26f9ffc704e8e901383b6a3a2c100a1
haci1995 <hajiaghaaa@code.edu.az>
2024-06-08 08:39:16 +0400

    first

---
+++ Task/Shofy/Models/ProductImage.cs
@@ -0,0 +1,10 @@
+﻿namespace Shofy.Models
+{
+    public class ProductImage
+    {
+        public string Name { get; set; }
+        public bool IsMain { get; set; } = false;
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+}
